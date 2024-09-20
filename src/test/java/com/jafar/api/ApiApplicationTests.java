ff26fb72b7b0de1f99660d1947cdfaa7be68c293
horang <fnelclsrn123@gmail,com>
2024-09-20 21:11:43 +0900

    feat: 초기 세팅

---
+++ src/test/java/com/jafar/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jafar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
