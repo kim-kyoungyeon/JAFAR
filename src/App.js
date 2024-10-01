62bb03a2615cb60a011457fba666934ba8692948
kim-kyoungyeon <marycarychin@gmail.com>
2024-10-02 00:51:33 +0900

    [ADD] : changed deploy, padd node 16.20.2 engine to package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import TestTuiEditor from './Pages/TestTuiEditor';
-
+import logo from "./logo.svg";
+import "./App.css";
+import TestTuiEditor from "./Pages/TestTuiEditor";
+import Editor from "./Pages/Editor";
 function App() {
   return (
     <div>
       <TestTuiEditor />
+      <Editor />
     </div>
   );
 }
---
+++ src/Pages/js/theme/black-theme.js
@@ -0,0 +1,19 @@
+// import Editor from "../../Editor";
+
+// const { Editor } = toastui;
+
+// const editor = new Editor({
+//   el: document.querySelector("#editor"),
+//   previewStyle: "vertical",
+//   height: "500px",
+//   initialValue: content,
+//   theme: "dark",
+// });
+
+// const viewer = Editor.factory({
+//   el: document.querySelector("#viewer"),
+//   viewer: true,
+//   height: "500px",
+//   initialValue: content,
+//   theme: "dark",
+// });
