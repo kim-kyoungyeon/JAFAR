f34eb576059af79f08b08c1cee4c7b34b6647438
kim-kyoungyeon <marycarychin@gmail.com>
2024-10-04 10:35:44 +0900

    ✌️  FIX : git conflict fix

--- src/components/TestTuiEditor.js
+++ src/components/TestTuiEditor.js
@@ -6,65 +6,43 @@ import "../styles/editor.css";
 import sampleLogo from "../styles/sampleLogo.jpg";
 
 const TestTuiEditor = () => {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
- 
   return (
     <div className="editor-container">
       <div className="main-content">
         <header className="header">
-          <img src= {sampleLogo} width='50px'/>
+          <img src={sampleLogo} width="50px" />
           <div className="header-buttons">
             <button className="button">내보내기</button>
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-  const [isLoggedIn, setIsLoggedIn] = useState(true); // 예시를 위해 true로 설정
-
-  return (
-    <EditorContainer>
-      <MainContent>
-        <Header>
-          <img src={sampleLogo} width="50px" />
-          <div>
-            <Button>내보내기</Button>
->>>>>>> Stashed changes
           </div>
         </header>
-          <ImageEditor
-            includeUI={{
-              menu: [
-                "crop",
-                "flip",
-                "rotate",
-                "draw",
-                "shape",
-                "icon",
-                "text",
-                "mask",
-                "filter",
-              ],
-              initMenu: "filter",
-              uiSize: {
-                width: "100%",
-                height: "calc(100vh - 60px)", 
-              },
-              menuBarPosition: "left",
-            }}
-            cssMaxHeight={500}
-            cssMaxWidth={700}
-            selectionStyle={{
-              cornerSize: 20,
-              rotatingPointOffset: 70,
-            }}
-            usageStatistics={true}
-          />
+        <ImageEditor
+          includeUI={{
+            menu: [
+              "crop",
+              "flip",
+              "rotate",
+              "draw",
+              "shape",
+              "icon",
+              "text",
+              "mask",
+              "filter",
+            ],
+            initMenu: "filter",
+            uiSize: {
+              width: "100%",
+              height: "calc(100vh - 60px)",
+            },
+            menuBarPosition: "left",
+          }}
+          cssMaxHeight={500}
+          cssMaxWidth={700}
+          selectionStyle={{
+            cornerSize: 20,
+            rotatingPointOffset: 70,
+          }}
+          usageStatistics={true}
+        />
       </div>
       <div className="right-sidebar">
         <h3>생성형 이미지 추천</h3>
